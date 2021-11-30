11cfcd39832b3687e98ac977a47c3654e16c51f0
Kishan <k.sharma8321@gmail.com>
2021-11-30 23:54:48 +0000

    text surface implementation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from window_sizing import resize_surface
+from window_sizing import resize_surface, resize_text
 
 def game_loop(screen):
     pygame.display.set_caption("Epic chess engine")
@@ -8,7 +8,8 @@ def game_loop(screen):
     # the actual surface dimensions are set in the resize event
     size_placeholder = (1, 1)
 
-    # -- initialise objects --
+
+    # -- initialise surfaces --
     window = pygame.Surface(size_placeholder)
 
     chess_board = pygame.Surface(size_placeholder)
@@ -19,24 +20,18 @@ def game_loop(screen):
     eval_minimiser = pygame.Surface(size_placeholder)  # user, displayed beneath the engine
 
     options_border = pygame.Surface(size_placeholder)
+    text_output = pygame.Surface(size_placeholder)
 
-    # post the first resize event
+    # -- post resize events --
     pygame.event.post(pygame.event.Event(pygame.VIDEORESIZE, {'w': 600, 'h': 400}))
+    # second post allows for rendering of text on the now non-zero sized surfaces
+    pygame.event.post(pygame.event.Event(pygame.VIDEORESIZE, {'w': 600, 'h': 400}))
+
+
     # -- game loop --
     while True:
 
-        # -- color surfaces --
         screen.fill((6, 65, 76))
-        window.fill((23, 89, 101))
-
-        chess_board.fill((231, 167, 106))
-        chess_board_border.fill((0, 0, 0))
-
-        eval_bar.fill((0, 0, 0))
-        eval_maximiser.fill((231, 118, 106))
-        eval_minimiser.fill((80, 175, 101))
-
-        options_border.fill((0, 0, 0))
 
         # -- event handler --
         for event in pygame.event.get():
@@ -45,20 +40,34 @@ def game_loop(screen):
 
             # -- resize event --
             if event.type == pygame.VIDEORESIZE:
+
+                # -- color surfaces --
+                window.fill((23, 89, 101))
+                chess_board.fill((231, 167, 106))
+                chess_board_border.fill((0, 0, 0))
+
+                eval_bar.fill((0, 0, 0))
+                eval_maximiser.fill((231, 118, 106))
+                eval_minimiser.fill((80, 175, 101))
+
+                options_border.fill((0, 0, 0))
+
+                # -- resize surfaces --
                 screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
 
-                window, window_pos = resize_surface(1, window, screen, (13, 9), (0.5, 0.5))
+                window, window_pos = resize_surface(1, window, screen, (16, 9), (0.5, 0.5))
 
-                chess_board_border, chess_board_border_pos = resize_surface(0.9, chess_board_border, window, (1, 1), (0.4, 0.5))
+                chess_board_border, chess_board_border_pos = resize_surface(0.9, chess_board_border, window, (1, 1), (0.35, 0.5))
                 chess_board, chess_board_pos = resize_surface(0.95, chess_board, chess_board_border, (1, 1), (0.5, 0.5))
 
-                eval_bar, eval_bar_pos = resize_surface(1, eval_bar, window, (1, 19), (0.03, 0.5))
-                eval_maximiser, eval_maximiser_pos = resize_surface(0.9, eval_maximiser, eval_bar, (1, 15), (0.5, 0.25))
-                eval_minimiser, eval_minimiser_pos = resize_surface(0.9, eval_minimiser, eval_bar, (1, 15), (0.5, 0.75))
+                eval_bar, eval_bar_pos = resize_surface(0.95, eval_bar, window, (1, 14), (0.045, 0.5))
+                eval_maximiser, eval_maximiser_pos = resize_surface(0.8, eval_maximiser, eval_bar, (1, 15), (0.5, 0.25))
+                eval_minimiser, eval_minimiser_pos = resize_surface(0.8, eval_minimiser, eval_bar, (1, 15), (0.5, 0.75))
 
-                options_border, options_border_pos = resize_surface(0.9, options_border, window, (4, 11), (0.85, 0.5))
+                options_border, options_border_pos = resize_surface(0.9, options_border, window, (4, 6), (0.8, 0.5))
+                text_output, text_output_pos = resize_text(0.9, text_output, options_border, (5, 2), (0.5, 0.3))
+
         # -- blit surfaces --
-
         chess_board_border.blit(chess_board, (chess_board_pos))
         window.blit(chess_board_border, (chess_board_border_pos))
 
@@ -66,6 +75,7 @@ def game_loop(screen):
         eval_bar.blit(eval_minimiser, (eval_minimiser_pos))
         window.blit(eval_bar, (eval_bar_pos))
 
+        options_border.blit(text_output, text_output_pos)
         window.blit(options_border, (options_border_pos))
 
         screen.blit(window, (window_pos))
@@ -75,6 +85,7 @@ def game_loop(screen):
 
 if __name__ == "__main__":
     pygame.display.init()
+    pygame.freetype.init()
     main_screen = pygame.display.set_mode((1, 1), pygame.RESIZABLE)
     game_loop(main_screen)
     pygame.quit()
--- window_sizing.py
+++ window_sizing.py
@@ -1,6 +1,7 @@
 import pygame
+from pygame import freetype
 
-def expand_to_fill_maintaining_ratio(smaller, larger, ratio):
+def expand_fill_maintaining_ratio(smaller, larger, ratio):
     """returns a surface resized to fill the larger surface while maintaining the ratio"""
     maximum_size = list(larger.get_size())
     i = 0
@@ -18,16 +19,17 @@ def expand_to_fill_maintaining_ratio(smaller, larger, ratio):
 
 
 def resize_surface(shrink, surf, outer_surf, ratio, alignment):
+
+    # "shrink" is a float from 0-1 determining how much of the outer surface the inner will fill
     if (type(shrink) != float and shrink != 1) or not(0 < shrink <= 1):
         raise ValueError("shrink must be float between 0 and 1")
 
     # -- create padding between outer and inner --
     # create a shrunk outer surface to use instead, so when the inner surface is expanded there will be some gap
-    # "shrink": float from 0-1 determining how much of the outer surface the inner will fill
     shrunk_outer_surf = pygame.transform.scale(outer_surf, (int(outer_surf.get_width() * shrink),
                                                             int(outer_surf.get_height() * shrink)))
 
-    surf = expand_to_fill_maintaining_ratio(surf, shrunk_outer_surf, ratio)
+    surf = expand_fill_maintaining_ratio(surf, shrunk_outer_surf, ratio)
 
     # -- center smaller surface --
     surf_pos = (int(outer_surf.get_width() * alignment[0]), int(outer_surf.get_height() * alignment[1]))
@@ -35,3 +37,19 @@ def resize_surface(shrink, surf, outer_surf, ratio, alignment):
     surf_rect.centerx, surf_rect.centery = surf_pos
 
     return surf, surf_rect
+
+
+def resize_text(shrink, surf, outer_surf, ratio, alignment):
+
+    surf.fill((255, 10, 10))
+    surf, surf_pos = resize_surface(shrink, surf, outer_surf, ratio, alignment)
+
+    # -- generate and blit text --
+    font = pygame.freetype.SysFont("bell", surf.get_height())
+    if surf.get_size() == (0, 0):
+        text_surf, text_rect = font.render("test", fgcolor=(255, 255, 255), size=60)
+    else:
+        text_surf, text_rect = font.render("test", fgcolor=(255, 255, 255), size=surf.get_height()*0.7)
+    surf.blit(text_surf, text_surf.get_rect(center=surf.get_rect().center))
+
+    return surf, surf_pos
